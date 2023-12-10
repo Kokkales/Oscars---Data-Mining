29fa68197e094c413b4c15246cd1998bcc2fb373
Konstantinos Kokkalis <kokkaleskonstantinos@gmail.com>
2023-12-11 00:50:10 +0200

    UPDATE: preprocessing
    - numeric to nominal
    - still need to fix and clean code

--- datapre.py
+++ datapre.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MultiLabelBinarizer
+from textblob import Word
 from imdb import IMDb
 from openpyxl import load_workbook, Workbook
 
@@ -13,6 +17,122 @@ from openpyxl import load_workbook, Workbook
 #   print(f"Format file: {file}")
 #   return file
 
+
+def uniqueGenres(file):
+
+  file['genre'] = file['genre'].apply(lambda x: ' '.join([Word(word).correct().lower() for word in x.split()]))
+  file['genre'] = file['genre'].apply(lambda x: re.sub(r'[^a-zA-Z,\s]', '', x))
+# Save the corrected file back to the Excel file
+  file.to_excel('new.xlsx', index=False)
+  print("difficult job done ")
+
+  all_genres = []
+  for genres in file['genre']:
+      if isinstance(genres, str):  # Check if the value is a string
+          genres_list = re.split(',| ', genres)  # Split by comma or space
+          all_genres.extend([genre.strip() for genre in genres_list if genre.strip()])  # Remove empty strings and strip spaces
+
+  # Get unique genres
+  unique_genres = list(set(all_genres))
+  correct_unique=[]
+  for word in unique_genres:
+    correctWord=Word(word).correct().lower()
+    correct_unique.append(str(correctWord))
+
+  # Display unique genres
+  # print(unique_genres)
+  # print(correct_unique)
+  unique_genres=list(set(correct_unique))
+  # print(unique_genres)
+
+  unique_words = list(unique_genres)
+  for i in range(len(unique_genres)):
+    for j in range(i + 1, len(unique_genres)):
+        match_count = 0
+        for k in range(min(len(unique_genres[i]), len(unique_genres[j]))):
+            if unique_genres[i][k] == unique_genres[j][k]:
+                match_count += 1
+                if match_count > 3:
+                    # print(unique_genres[i],unique_genres[j])
+                    if unique_genres[i] in unique_words:
+                        unique_words.remove(unique_genres[i])
+                    elif (unique_genres[j] in unique_words):
+                        unique_words.remove(unique_genres[j])
+                    break
+            else:
+                break
+
+  print("Original list:", unique_genres)
+  print("Updated list:", unique_words)
+
+  return unique_words
+
+TYPES=['adaptation','screenplay','original','based on a true story','sequel','remake']
+def oneHotEncoding(file):
+   print(file.shape)
+   # SCRIPT TYPE
+   print(TYPES)
+  #  for type in TYPES:
+  #     # TODO fix the split method
+  #     file[type] = file['script type'].apply(lambda )
+   segment_text = lambda text, types: \
+    (lambda r_text, res: res if not r_text else r_text[1:].strip() and segment_text(r_text[1:].strip(), types) if not any(r_text.startswith(t) for t in sorted(types, key=len, reverse=True)) else segment_text(r_text[len([t for t in sorted(types, key=len, reverse=True) if r_text.startswith(t)][0]):].strip(), types))(text.strip(), [])
+   for t in TYPES:
+    file[t] = file['script type'].apply(lambda x: 1 if t in segment_text(x, TYPES) else 0)
+
+   file=dropUseless(file,['script type'])
+
+
+  # GENRE
+   genres=set()
+
+   for filmGenres in file['genre']:
+      corrected_genres = []
+      for genre in filmGenres.split():
+          corrected_genre = str(Word(genre).correct().lower())
+          corrected_genres.append(corrected_genre)
+      # TODO FIX IT THE SPELLING THREE LETTERS SIMILARITY
+      final_genres = []
+      for genre in corrected_genres:
+        if len(final_genres) == 0:
+            final_genres.append(genre)
+        else:
+            last_genre = final_genres[-1]
+            if len(set(genre) - set(last_genre)) <= 1:
+                consecutive_count = 0
+                for i in range(len(genre) - 2):
+                    if genre[i] == genre[i + 1] == genre[i + 2]:
+                        consecutive_count += 1
+                if consecutive_count < 3:
+                    final_genres.append(genre)
+            else:
+                final_genres.append(genre)
+
+      genres.update(final_genres)
+      # genres.update(corrected_genres.split())
+
+   print(genres)
+   for genre in genres:
+      file[genre] = file['genre'].apply(lambda x: 1 if genre in x.split() else 0)
+
+   file=dropUseless(file,['genre'])
+
+   # DATE
+   file['release date (us)'] = pd.to_datetime(file['release date (us)'], format='mixed')
+   dateColumns = pd.DataFrame({
+    'Month': file['release date (us)'].dt.month.astype(str),
+    'Day': file['release date (us)'].dt.day.astype(str),
+    'Year': file['release date (us)'].dt.year.astype(str)
+    })
+   encoded_dates = pd.get_dummies(dateColumns).astype(int)
+   file = pd.concat([file, encoded_dates], axis=1)
+   file=dropUseless(file,['release date (us)'])
+  #  print(file)
+   print(file.shape)
+  #  print(file['release date (us)'])
+   return file
+
+PRECENTAGES=['of gross earned abroad','budget recovered','budget recovered opening weekend']
 def precentagesToDecimal(file):
   #  Convert each value of the df that is ending with the '%' to decimal (divide by 100)
   file = file.map(lambda val: float(val.rstrip('%')) / 100 if isinstance(val, str) and val.endswith('%') else val)
@@ -32,8 +152,8 @@ def dropUseless(file,uselessColumns):
 
 
 def deleteDuplicate(file):
-  print(f'The dataset contains {df.duplicated().sum()} duplicate rows that need to be removed.')
-  df.drop_duplicates(inplace=True)
+  print(f'The dataset contains {file.duplicated().sum()} duplicate rows that need to be removed.')
+  file.drop_duplicates(inplace=True)
   print(f"DUPLICATE ROWS HAVE BEEN SUCCESFULLY DELETED!")
   return file
 
@@ -41,23 +161,14 @@ def externalKnowledge(file):
   ia = IMDb()
   # test=print(df['Film'])
   i=2
-  for movieTitle in file['Film']:
+  for movieTitle in file['film']:
     movies = ia.search_movie(f"{movieTitle}")
-    # movies = ia.search_movie(f"")
-    # Get details of the first search result
     try:
       if movies:
           movie = ia.get_movie(movies[0].movieID)
-
-          # Print movie details
-          # print("Movie Details:")
           print(f"Title: {movie['title']}")
-          # print(f"Year: {movie['year']}")
           year=movie['year']
-          # print(f"Rating: {movie['rating']}")
           rating=movie['rating']
-          # print(f"Genres: {', '.join(movie['genres'])}")
-          # print(f"Plot: {movie['plot'][0]}")
       else:
           print("Movie not found.")
           continue
@@ -70,102 +181,104 @@ def externalKnowledge(file):
     except:
       file.at[i-2, 'imdb rating'] = np.nan
       continue
-    # file.to_excel("clone.xlsx")
     print(file.head(5))
     i=i+1
   print(f'EXTERNAL KNOWLEDGE \'IMDb\' HAS BEEN SUCCESFULLY ADDED')
+  # file.to_excel("clone.xlsx")
   return file
 
-def missingValues(file):
-  COMMA_COL=['average critics ','average audience ','opening weekend','domestic gross','foreign gross','worldwide gross',' of gross earned abroad','budget ($million)',' budget recovered',' budget recovered opening weekend']
-  file[COMMA_COL] = file[COMMA_COL].replace(',', '', regex=True)
-  for index, row in file.iterrows():
+
+STRING_COL=['script type','genre','oscar winners']
+def stringMissingValues(file):
+   for index, row in file.iterrows():
+      # oscar winners
+      if (pd.isnull(row['oscar winners'])):
+         file.loc[index, 'oscar winners'] = 0
+      else:
+         file.loc[index, 'oscar winners'] = 1
       # Get the year of the film
       year = row['year']
 
-      # Calculate mean for films created in the same year
-      # mean of average critics
-      mean_critics = file[file['year'] == year]['average critics '].mean()
-      file['average critics '] = pd.to_numeric(df['average critics '], errors='coerce')
-      mean_audience = file[file['year'] == year]['average audience '].mean()
-      file['average audience '] = pd.to_numeric(df['average audience '], errors='coerce')
-      mean_opening = file[file['year'] == year]['opening weekend'].mean()
-      file['opening weekend'] = pd.to_numeric(df['opening weekend'], errors='coerce')
-      mean_domestic = file[file['year'] == year]['domestic gross'].mean()
-      file['domestic gross'] = pd.to_numeric(df['domestic gross'], errors='coerce')
-      mean_foreign = file[file['year'] == year]['foreign gross'].mean()
-      file['foreign gross'] = pd.to_numeric(df['foreign gross'], errors='coerce')
-      mean_worldwide = file[file['year'] == year]['worldwide gross'].mean()
-      file['worldwide gross'] = pd.to_numeric(df['worldwide gross'], errors='coerce')
-      mean_abroad = file[file['year'] == year][' of gross earned abroad'].mean()
-      file[' of gross earned abroad'] = pd.to_numeric(df[' of gross earned abroad'], errors='coerce')
-      mean_budget = file[file['year'] == year]['budget ($million)'].mean()
-      file['budget ($million)'] = pd.to_numeric(df['budget ($million)'], errors='coerce')
-      mean_budget_rec = file[file['year'] == year][' budget recovered'].mean()
-      file[' budget recovered'] = pd.to_numeric(df[' budget recovered'], errors='coerce')
-      mean_budget_rec_open = file[file['year'] == year][' budget recovered opening weekend'].mean()
-      file[' budget recovered opening weekend'] = pd.to_numeric(df[' budget recovered opening weekend'], errors='coerce')
+      # Genre
+      # file['genre']=file['genre'].str.lower()
+      if pd.isnull(row['genre']):
+        ia=IMDb()
+        try:
+          # print("::",row['film'])
+          movies = ia.search_movie(row['film'])
+          if movies: #TODO fix it
+              movie = ia.get_movie(movies[0].movieID)
+              genre=",".join(movie['genres']).lower()
+              if not genre:
+                  file.at[index, 'genre'] = np.nan
+              else:
+                  file.at[index, 'genre'] = genre
+          else:
+              print("Movie not found.")
+              genre=np.nan
+        except:
+            file['genre']=file['genre'].fillna(method='ffill')
+            # file.at[index, 'genre'] = np.nan
+      if pd.isnull(row['script type']):
+         file['script type']=file['script type'].fillna(method='ffill')
+   file['genre'] = file['genre'].str.replace(',', ' ').str.replace('.', ' ').str.replace('\s+', ' ', regex=True).str.strip()
+   file['script type'] = file['script type'].str.replace(',', ' ').str.replace('.', ' ').str.replace('\s+', ' ', regex=True).str.strip()
 
-      # Fill missing value with the mean for that year
-      if pd.isna(row['average critics ']):
-          file.loc[index, 'average critics '] = mean_critics
-      if pd.isna(row['average audience ']):
-          file.loc[index, 'average audience '] = mean_audience
-      if pd.isna(row['opening weekend']):
-          file.loc[index, 'opening weekend'] = mean_opening
-      if pd.isna(row['domestic gross']):
-          file.loc[index, 'domestic gross'] = mean_domestic
-      if pd.isna(row['foreign gross']):
-          file.loc[index, 'foreign Gross'] = mean_foreign
-      if pd.isna(row['worldwide gross']):
-          file.loc[index, 'worldwide gross'] = mean_worldwide
-      if pd.isna(row[' of gross earned abroad']):
-          file.loc[index, ' of gross earned abroad'] = mean_abroad
-      if pd.isna(row['budget ($million)']):
-          file.loc[index, 'budget ($million)'] = mean_budget
-      if pd.isna(row[' budget recovered']):
-          file.loc[index, ' budget recovered'] = mean_budget_rec
-      if pd.isna(row[' budget recovered opening weekend']):
-          file.loc[index, ' budget recovered opening weekend'] = mean_budget_rec_open
-      # print("HPPP",index)
-      # file.to_excel('test.xlsx')
-      # print(index)
-  print(mean_opening)
+   print("MISSING VALUES HAS BEEN SUCCESFULLY RESTORED! (other)")
+
+   return file
+
+
+COMMA_COL=['average critics','average audience','opening weekend','domestic gross','foreign gross','worldwide gross','of gross earned abroad','budget ($million)','budget recovered','budget recovered opening weekend','imdb rating']
+def numericalMissingValues(file):
+  file[COMMA_COL] = file[COMMA_COL].replace(',', '', regex=True)
+  for element in COMMA_COL:
+      for index, row in file.iterrows():
+        year = row['year']
+        mean = file[file['year'] == year][element].mean()
+        file[element] = pd.to_numeric(df[element], errors='coerce')
+        if pd.isna(row[element]):
+          file.loc[index, element] = mean
+  print(f"MISSING VALUES HAS BEEN SUCCESFULLY RESTORED!")
   return file
 
+# NUMERICAL=['average critics','average audience','opening weekend','domsestic gross','foreign gross','worldwide gross','budget ($million)','imdb rating']
+# def formatNumerical(file):
+#     for col in NUMERICAL:
+#         # Replace commas with dots in the specified columns
+#         if col in file.columns:
+#             file[col] = file[col].str.replace(',', '.')
+#             file[col] = pd.to_numeric(file[col], errors='coerce')  # Convert values to decimal
+#         else:
+#            raise ValueError(f"Column with the name {col} doesn;t exist.")
+#     print("NUMBERS HAVE BEEN SUCESFULLY FORMATED!")
+#     return file
+
+
+
 # DATA PREPROCESSING
-# 1. Open Main excel using pandas
-df = pd.read_excel('movies.xlsx')
-# df.columns = df.columns.str.lower()
+df=pd.read_excel('Book.xlsx', sheet_name = 'Sheet1',na_values=['-'])
+print(df.head())
 df.columns = df.columns.str.lower().str.replace(r'\s+', ' ', regex=True)
+df.columns = df.columns.str.strip()
+df.to_excel('Book.xlsx', index=False)
+# print(df.shape)
 
-# Save the modified DataFrame back to Excel
-df.to_excel('testing.xlsx', index=False)
-# df.iloc[0] = df.iloc[0].astype(str).str.lower()
-# Save the modified DataFrame back to Excel
-# df.to_excel('testing.xlsx', index=False)
-
-df=pd.read_excel('testing.xlsx', sheet_name = 'Sheet1',na_values=['-'])
-print(df.shape)
-
-#TODO 2. Format Column names
-# df=columnFormat(df)
 # 3. POINT USELESS COLUMNS
-USELESS_COL=['rotten tomatoes critics','metacritic critics','rotten tomatoes audience ','metacritic audience ','rotten tomatoes vs metacritic deviance','audience vs critics deviance ','primary genre','opening weekend ($million)','domestic gross ($million)','foreign gross ($million)','worldwide gross ($million)','distributor','imdb vs rt disparity','oscar detail']
-# print(len(USELESS_COL))
+USELESS_COL=['rotten tomatoes critics','metacritic critics','rotten tomatoes audience','metacritic audience','rotten tomatoes vs metacritic deviance','audience vs critics deviance','primary genre','opening weekend ($million)','domestic gross ($million)','foreign gross ($million)','worldwide gross ($million)','distributor','imdb vs rt disparity','oscar detail']
 df=dropUseless(df,USELESS_COL) # 4. DELETE USELESS COLUMNS
-print(f"New shape: {df.shape}")
 # externalKnowledge(df) # 5. RETRIEVE EXTERNAL KNOWLEDGE, ADDITIONAL DATA
 df=precentagesToDecimal(df)# 6. CONVERT PRECENTAGES CELLS TO DECIMAL
-# TODO7. REPLACE MISSING VALUES WITH THE COLUMN MEAN
-df=missingValues(df)
+df=numericalMissingValues(df)
+df=stringMissingValues(df)
+# df=formatNumerical(df)
 # TODO8. CONVERT NUMERICAL TO NOMINAL VALUES
+df=oneHotEncoding(df)
 
-df=deleteDuplicate(df)  # 9. CHECK FOR DUPLICATE ROWS
+# df=deleteDuplicate(df)  # 9. CHECK FOR DUPLICATE ROWS
 df=dropUseless(df,['film','year']) # 4. DELETE USELESS COLUMNS
-print(df.shape)
-
-df.to_excel("clone.xlsx") #10. Convert pandas updated dataset to a new excel with the final data
-# print(df.columns)
-missing_data = pd.read_excel('clone.xlsx').isnull().sum()
+print(df.head(5))
+df.to_excel("datesThree.xlsx") #10. Convert pandas updated dataset to a new excel with the final data
+missing_data = pd.read_excel('datesThree.xlsx').isnull().sum()
 print(f"# of missing data: {missing_data}")
+print(f"--------------------------FINISHED-------------------------")
